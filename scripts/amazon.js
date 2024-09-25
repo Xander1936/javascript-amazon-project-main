1c32a3901bf3f87fb70f371ab24769b1078089d5
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2024-09-25 13:17:21 +0100

    Data structure to be used by our JavaScript File

---
+++ scripts/amazon.js
@@ -0,0 +1,36 @@
+// Main Idea Of Javascript
+// 1. Save the data : data = infos about our products
+// 2. Generate the HTML
+// 3. Make it interactive
+
+// Step 1: Save the data
+// This is called a data structure.
+const products = [
+{
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090
+},
+{
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    priceCents: 2095
+},
+{
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCents: 799
+}
+];
